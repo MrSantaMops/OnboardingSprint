a7e735107936f9f1b4da8e667ef261733e0addef
MrHomka <candle.littlegg@gmail.com>
2026-02-18 00:43:06 +0700

    Test1_1

---
+++ src/main/kotlin/level_1/test_1.kt
@@ -0,0 +1,7 @@
+package org.example.level_1
+
+fun main(){
+    val ordersCompleted: Int = 75
+    println("$ordersCompleted, $ACTION_FOR_SUCCES_FUL_PAYMENT")
+}
+const val ACTION_FOR_SUCCES_FUL_PAYMENT: String = "Thank you for forgetting how to walk"
