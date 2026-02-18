10c2c9edab838781901164c3036e3b9699d5fe8d
MrHomka <candle.littlegg@gmail.com>
2026-02-18 14:42:41 +0700

    UpdateTime

--- src/main/kotlin/level_1/test_5.kt
+++ src/main/kotlin/level_1/test_5.kt
@@ -2,7 +2,7 @@ package org.example.level_1
 
 fun main(){
 
-    val inGeneralTimeInSpaceSec: Int = 3805
+    val inGeneralTimeInSpaceSec: Int = 6480
 
     val timeInSpaceSec = (inGeneralTimeInSpaceSec % 60).toString().padStart(2, '0')
     val timeInSpaceMin = (inGeneralTimeInSpaceSec % 3600 / 60).toString().padStart(2, '0')
