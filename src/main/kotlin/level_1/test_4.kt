8953a7e1133612becdd3f690c4fd32a18ad24f62
MrHomka <candle.littlegg@gmail.com>
2026-02-18 14:03:42 +0700

    Test1_4

---
+++ src/main/kotlin/level_1/test_4.kt
@@ -0,0 +1,16 @@
+package org.example.level_1
+
+fun main(){
+    val length: ULong = 40_868_600_000u
+    val age: UByte = 27u
+    val partOfDay: Double = 0.075
+    val seconds: UShort = 6480u
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: UInt = 327000u
+
+    println("Age: $age")
+    println("Part: $partOfDay")
+    println("Seconds: $seconds")
+    println("PartOfYear: $partOfYear")
+    println("Apogee: $apogee")
+}
