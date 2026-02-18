3c62893ff2e7aec2e691ba078ad3dde4ed3028bc
MrHomka <candle.littlegg@gmail.com>
2026-02-18 13:47:44 +0700

    Test1_3

---
+++ src/main/kotlin/level_1/test_3.kt
@@ -0,0 +1,12 @@
+package org.example.level_1
+
+fun main(){
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 5
+    println("Data about the first human spaceflight by Yuri Gagarin")
+    println("Year of flight - $year\nTake-off:\nHour - $hour\nMinute - $minute")
+    hour = 10
+    minute = 55
+    println("Landing: $hour:$minute ") //Подсказке-print не нашел применения
+}
