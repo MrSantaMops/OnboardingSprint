f9c8cae8916ba8a300fe0c65a85146ce04acd533
MrHomka <candle.littlegg@gmail.com>
2026-02-18 13:49:37 +0700

    Test1_3

--- src/main/kotlin/level_1/test_3.kt
+++ src/main/kotlin/level_1/test_3.kt
@@ -6,6 +6,7 @@ fun main(){
     var minute: Int = 5
     println("Data about the first human spaceflight by Yuri Gagarin")
     println("Year of flight - $year\nTake-off:\nHour - $hour\nMinute - $minute")
+
     hour = 10
     minute = 55
     println("Landing: $hour:$minute ") //Подсказке-print не нашел применения
