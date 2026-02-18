d1149739a0834e6554e1bbf8bc91b12621e229ab
MrHomka <candle.littlegg@gmail.com>
2026-02-18 13:27:37 +0700

    Test1_2

---
+++ src/main/kotlin/level_1/test_2.kt
@@ -0,0 +1,12 @@
+package org.example.level_1
+
+fun main(){
+    val ordersCompleted: Int = 75
+    println("$ordersCompleted, $ACTION_FOR_SUCCES_FUL_PAYMENT")
+
+    var companyStaff: Int = 2000
+    println("$companyStaff - for the period up to (company staff)")
+
+    companyStaff --
+    println("$companyStaff - for the period after (company staff)")
+}
