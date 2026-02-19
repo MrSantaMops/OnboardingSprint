db13b0409469e61d87b2fd7ca5b8801d9c0b8581
MrHomka <candle.littlegg@gmail.com>
2026-02-19 21:47:55 +0700

    KS-1-1-removed comments

--- src/main/kotlin/level_1/test_1.kt
+++ src/main/kotlin/level_1/test_1.kt
@@ -2,6 +2,6 @@ package org.example.level_1
 
 fun main(){
     val ordersCompleted: Int = 75
-    println("$ordersCompleted, $ACTION_FOR_SUCCES_FUL_PAYMENT") //проверка
+    println("$ordersCompleted, $ACTION_FOR_SUCCES_FUL_PAYMENT")
 }
 const val ACTION_FOR_SUCCES_FUL_PAYMENT: String = "Thank you for forgetting how to walk"
